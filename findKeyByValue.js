5e4eac7891cdb3f0d472e83b6891e361e9940ac8
eusoncode <git@github.com:eusoncode/Kata.git>
2023-06-18 11:04:47 -0600

    All

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,15 +1,15 @@
-const findKeyByValue = function (obj, val) {
+const findKeyByValue = function(obj, val) {
   for (let key in obj) {
-    if (obj.hasOwnProperty(key) && obj[key] === val) {
+    if (key in obj && obj[key] !== undefined && obj[key] === val) {
       return key;
     }
   }
   return null; // Return null if no key is found
-}
+};
 
 // Example usage
 const obj = { name: "Eustace", age: 30, city: "New York" };
 const valueToFind = "Eustace";
-const key = findKeyByValue(obj, valueToFind);
+// const key = findKeyByValue(obj, valueToFind);
 
 console.log(findKeyByValue(obj, valueToFind));
