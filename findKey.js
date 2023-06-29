f51cc37e1e2853c4a64cad3cf1fe8f90d0b76d42
eusoncode <git@github.com:eusoncode/Kata.git>
2023-06-28 21:54:55 -0600

    Implement findKey

---
+++ findKey.js
@@ -0,0 +1,24 @@
+// Define the FindKey function
+const findKey = function(object, callback) {
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+};
+
+//Define assert equals
+const assertEqual = (actual, expected) => {
+  (actual === expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma');
+
+
