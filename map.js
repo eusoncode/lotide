f476e9bef87d2794b746dbca486af6027d784e42
eusoncode <git@github.com:eusoncode/Kata.git>
2023-06-28 20:06:06 -0600

    Implement Map

---
+++ map.js
@@ -0,0 +1,44 @@
+//Define eqArrays function that takes two arguments
+const eqArrays = function(arg1, arg2) {
+  let newArray1 = [];
+  let newArray2 = [];
+
+  //Check if the length of both arrays match
+  if (arg1.length !== arg2.length) {
+    return '❌❌❌ Both Arrays are not the same';
+  } else {
+    //Loop through the arrays and compare each element
+    for (let i = 0; i < arg1.length; i++) {
+      if (arg1[i] === arg2[i]) {
+        newArray1[i] = arg1[i];
+        newArray2[i] = arg2[i];
+      } else {
+        return '❌❌❌ Both Arrays are not the same';
+      }
+    }
+  }
+  //check if both arrays match
+  if (newArray1.toString() === newArray2.toString()) {
+    return '✅✅✅ Both Arrays are the same';
+  }
+};
+
+// Define the test function
+const assertArraysEqual = function(actual, expected) {
+  console.log(eqArrays(actual, expected));
+};
+
+//Define the map function
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']); // => Same arrays
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 'b']); // => Not Same arrays
+assertArraysEqual(map(words, word => word[0] + '*'), [ 'g*', 'c*', 't*', 'm*', 't*' ]); // => Same arrays
