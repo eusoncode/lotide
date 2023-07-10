80e8a2abdc8d677ea2c740014f215578258d8d6d
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-09 23:04:16 -0600

    Refactored tail

--- tail.js
+++ tail.js
@@ -1,20 +1,8 @@
+const assertEqual = require('./assertEqual');
+
 //RETURN TAIL OF AN ARRAY
 const tail = function(arg1) {
   return arg1.slice(1);
 };
 
-
-//FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TEST CODE
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
