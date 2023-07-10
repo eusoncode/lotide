84ba0a9ae55b5b9f2cd257173f30940267edc189
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-09 22:59:25 -0600

    Refactored assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,4 @@
 //FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = (actual, expected) => {(actual === expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`)};
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
+module.exports = assertEqual;
