9da434e1a8e41acc3d940f23c6dd46dd29707945
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-09 22:59:46 -0600

    Refactored head

--- head.js
+++ head.js
@@ -1,17 +1,8 @@
+const assertEqual = require('./assertEqual');
+
 //RETURN FIRST ITEM IN AN ARRAY
 const head = function(arg1) {
   return arg1[0];
 };
 
-//FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
