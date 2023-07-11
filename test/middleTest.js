9ddf5cf1068602109c1e42fc3ed0407bcfea8369
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-10 21:13:24 -0600

    modified

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,24 @@
 //Define assertArraysEqual function that takes two arguments
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
 
-//Define assertArraysEqual function that takes two arguments
+// //Define assertArraysEqual function that takes two arguments
+// const middle = require('../middle');
+
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]); // => Same arrays
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), 4); // => Same arrays
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [4, 5]); // => Same arrays
+
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]); // => Same arrays
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), 4); // => Same arrays
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [4, 5]); // => Same arrays
+describe("#tail", () => {
+  it("returns an array of the two middle numbers in the array [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]); // => Same arrays
+  });
+  it("returns the middle number of the array [1, 2, 3, 4, 5, 6, 7]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), 4); // => Same arrays
+  });
+  it("returns the middle number of the array [1, 2, 3, 4, 5, 6, 7, 8, 9]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), 5); // => Same arrays
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,21 @@
 // test/assertEqualTest.js
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// // TEST CODE
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// TEST CODE
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns 3 as the number of items in that array [Yo Yo, Lighthouse, Labs]", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words); // no need to capture the return value since we are not checking it
+    assert.deepEqual(words.length, 3); // original array should still have 3 elements!
+  });
+});
