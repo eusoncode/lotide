707f7f68304adb415b9ca4e082414c764e76898f
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-13 15:48:24 -0600

    Implemented takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -2,12 +2,13 @@
 const takeUntil = function(array, callbackNum) {
   let result = [];
   for (let item of array) {
-    if (!callbackNum(item)) {
-      result.push(item);
-    } else {
-      return result;
-    }
-  }
+    (!callbackNum(item)) ? result.push(item) : return result;
+  //   if (!callbackNum(item)) {
+  //     result.push(item);
+  //   } else {
+  //     return result;
+  //   }
+  // }
 };
 
 module.exports = takeUntil;
