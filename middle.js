c84ca17aecab8cf08e49aec4729cbaf35e06e84a
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-13 15:34:49 -0600

    fix empty inputs and made non-empty arrays, return a number in an array instead of just a number

--- middle.js
+++ middle.js
@@ -1,11 +1,11 @@
 const middle = function(array) {
   let middleItem = [];
-  if (array.length > 1) {
+  if (array.length > 2) {
     if (array.length % 2 === 0) {//even
       let value = array.length / 2;
       middleItem = array.slice(value - 1, value + 1);
     } else { //odd
-      return middleItem = array[Math.floor(array.length / 2)];
+      return [middleItem = array[Math.floor(array.length / 2)]];
     }
   } else {
     console.log('No middle item');
