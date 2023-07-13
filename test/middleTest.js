b394bdbb34b2d87e7d510c581966317c1874c83d
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-13 16:19:15 -0600

    Edited and tested the middle function

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,13 +2,13 @@ const assert = require('chai').assert;
 const middle = require('../middle');
 
 describe("#middle", () => {
-  it("returns an array of the two middle numbers in the array [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", () => {
+  it("returns an array of the two middle numbers [5, 6]", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]); // => Same arrays
   });
-  it("returns the middle number of the array [1, 2, 3, 4, 5, 6, 7]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), 4); // => Same arrays
+  it("returns the middle number [4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]); // => Same arrays
   });
-  it("returns the middle number of the array [1, 2, 3, 4, 5, 6, 7, 8, 9]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), 5); // => Same arrays
+  it("returns the middle number [5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]); // => Same arrays
   });
 });
